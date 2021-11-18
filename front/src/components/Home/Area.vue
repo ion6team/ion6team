bdaeae245ea26b7f4b29765ccd0941ebae101989
drfxsw <sedzaq@naver.com>
2021-11-18 21:06:25 +0900

    category 메뉴 디자인

--- front/src/components/Home/Area.vue
+++ front/src/components/Home/Area.vue
@@ -1,73 +1,72 @@
 <template>
-  
-  <b-container fluid >
-    <b-container
-      fluid="md"
-      class="p-4"
-      style="height:600px;"
-      >
-      {{this.$store.state.member.zzim}}zzz
-        <b-row align-v="center" style="height:100%">
-          <b-col cols='7'>
-            <b-row>
-              <h2>우리동네</h2>
-              <h2>중고직거래마켓</h2>
-              <p>동네 주민들과 가깝고 따뜻한 거래를 지금 경험해보세요.</p>
-            </b-row>
-            <b-row>
-              <b-col>
-                
-                <b-button href="/board"
-                class="bg-primary m-2 ">
+
+  <b-container fluid>
+    <b-container fluid="md" class="p-4" style="height:600px;">
+      {{this.$store.state.member.zzim}}
+      <b-row align-v="center" style="height:100%">
+        <b-col cols='7'>
+          <b-row>
+            <h2>우리동네</h2>
+            <h2>중고직거래마켓</h2>
+            <p>동네 주민들과 가깝고 따뜻한 거래를 지금 경험해보세요.</p>
+          </b-row>
+          <b-row>
+            <b-col>
+
+              <b-button @click="goboard()" class="m-2" style="background-color:#ff8a3d;">
                 내지역보기
-                </b-button>
+              </b-button>
 
-                <b-button
-                class="bg-primary m-2"
-                v-b-modal.needLogin>
-                버튼
-                </b-button>
+            </b-col>
+          </b-row>
+        </b-col>
 
-              </b-col>
-            </b-row>
-          </b-col>
-
-          <b-col cols='5'>
-              <img src="../../assets/map.png">
-          </b-col>
-       </b-row>
+        <b-col cols='5'>
+          <img src="../../assets/map.png">
+        </b-col>
+      </b-row>
 
       <div>
 
-        <b-modal id="needLogin" centered hide-footer>
-           <needLogin />
+        <b-modal id="needLogin" ref="my-modal" centered hide-footer>
+          <needLogin />
         </b-modal>
-  
-</div>
-  
-      
+
+      </div>
+
+  {{this.$store.state.islogin}}
 
     </b-container>
   </b-container>
 </template>
 
 <script>
-import NeedLogin from '../Modal/NeedLogin';
+  import NeedLogin from '../Modal/NeedLogin';
 
-export default {
-    name:'area',
-    components:{
+  export default {
+    name: 'area',
+    components: {
       NeedLogin
+    },
+    methods: {
+      goboard() {
+        if (this.$store.state.islogin==false) {
+          this.$refs['my-modal'].show()
+        } else {
+          this.$router.push("/board");
+        }
+      }
     }
-    
-}
+
+  }
 </script>
 
 <style scoped>
-h2{
-  font-size: 50px;
-}
-p{
-  margin: 20px 0;
-}
+  h2 {
+    font-size: 50px;
+  }
+
+  p {
+    margin: 20px 0;
+  }
 </style>
