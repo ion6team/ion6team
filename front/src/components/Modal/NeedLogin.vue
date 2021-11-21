5a4c64ec4f1635edf9bc948adc40403d8be01401
drfxsw <sedzaq@naver.com>
2021-11-22 02:52:03 +0900

    모달 디자인

--- front/src/components/Modal/NeedLogin.vue
+++ front/src/components/Modal/NeedLogin.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-      <p>로그인이 필요한 서비스입니다</p>
-      <b-link href="/login">로그인하러가기</b-link>
+      <div class="text-center" style=" padding-top:80px;">
+      <h5>로그인이 필요한 서비스입니다.</h5>
+      <b-button class="mt-3" href="/login" style="background-color:#ff8a3d; border-color:#fec69f;" >로그인하기</b-button>
+  </div>
   </div>
 </template>
 
--- front/src/components/Modal/Welcome.vue
+++ front/src/components/Modal/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-      <h3>환영합니다</h3>
-      <a href="/">홈으로</a>
+  <div class="text-center" style=" padding-top:80px;">
+      <h5>당근마켓에 오신것을 환영합니다</h5>
+      <b-button class="mt-3" href="/" style="background-color:#ff8a3d; border-color:#fec69f;" >홈으로</b-button>
   </div>
 </template>
 
