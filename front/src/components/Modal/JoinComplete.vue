49684e5d0e295aaf339a21ce5e65286ab0796360
drfxsw <sedzaq@naver.com>
2021-11-22 02:13:51 +0900

    새 비밀번호 입력 모달

--- front/src/components/Modal/JoinComplete.vue
+++ front/src/components/Modal/JoinComplete.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-      <h3>회원가입에 성공하셨습니다</h3>
-      <b-link href="/login">로그인하기</b-link>
+  <div class="text-center" style=" padding-top:80px;">
+      <h5>회원가입에 성공하셨습니다</h5>
+      <b-button class="mt-3" href="/login" style="background-color:#ff8a3d; border-color:#fec69f;" >로그인하기</b-button>
   </div>
 </template>
 
--- front/src/views/Join.vue
+++ front/src/views/Join.vue
@@ -213,9 +213,19 @@
         </validation-observer>
       </div>
     </div>
-    <b-modal id="joinComplete" ref="join-modal" centered hide-footer>
-      <join-complete />
-    </b-modal>
+    <b-modal id="joinComplete" ref="join-modal"  centered hide-header hide-footer style="position:relative;">
+<template #default="{ close }">
+                  <div style="background-color: #fbf7f2; min-height:200px; width:100%;">
+                      <b-button style="background-color:#ff8a3d; border:3px solid white; border-radius:50%; height:40px; width:40px; text0align:center; position:absolute; top:-10px; right:-10px;"
+                    @click="close()">
+                      X
+                    </b-button>
+                  
+                    <join-complete />
+
+                  </div>
+              </template>
+            </b-modal>
   </div>
 </template>
 
